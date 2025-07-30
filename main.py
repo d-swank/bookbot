27d8cf27778d7725952ae81a172f46d9b670ff22
d-swank <dakotaswank18@gmail.com>
2025-07-29 20:41:35 -0700

    add sys.argv import

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import number_of_words
 from stats import number_of_characters
 from stats import sorted_dictionaries
@@ -7,12 +8,16 @@ def get_book_text(filepath):
         return file.read()
 
 def main():
-    book_text = get_book_text('books/frankenstein.txt')
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_text = get_book_text(sys.argv[1])
     num_words = number_of_words(book_text)
     char_count = number_of_characters(book_text)
     sorted_char_count = sorted_dictionaries(char_count)
     print("\n============ BOOKBOT ============\n")
-    print(f"Analyzing book found at books/frankenstein.txt...\n")
+    print(f"Analyzing book found at {sys.argv[1]}...\n")
     print("----------- Word Count ----------\n")
     print(f"Found {num_words} total words\n")
     print("--------- Character Count -------\n")
